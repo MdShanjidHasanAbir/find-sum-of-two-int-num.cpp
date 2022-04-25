66234be81cb12909ffbc4dd724a86c611ed5a11d
Md. Shanjid Hasan Abir <100505657+MdShanjidHasanAbir@users.noreply.github.com>
2022-04-25 08:01:13 +0600

    Create find sum.cpp

---
+++ find sum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+int x,y,sum;
+void Sum()
+{ system( "CLS");
+    int sum = x+y;
+    cout<< "sum ="<<sum;
+}
+int main()
+{
+
+    cout<<"x =";
+    cin>>x;
+    cout<<"y =";
+    cin>>y;
+     Sum( );
+
+
+return 0;
+
+}
